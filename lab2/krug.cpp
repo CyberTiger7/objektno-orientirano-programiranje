324f0ae16ac6a87839149e694829a1f7239b2801
Krstev <krstev.kostadin@gmail.com>
2017-02-27 16:26:47 +0100

    initial commit

---
+++ lab2/krug.cpp
@@ -0,0 +1,55 @@
+//
+// Created by krstevkoki on 2/27/17.
+//
+#include <iostream>
+
+using namespace std;
+
+class Krug {
+private:
+    float radius;
+    const float PI = 3.14;
+public:
+    Krug() {
+        radius = 0;
+    }
+
+    Krug(float r) {
+        radius = r;
+    }
+
+    ~Krug() {
+
+    }
+
+    float const perimetar() {
+        return 2 * radius * PI;
+    }
+
+    float const plostina() {
+        return radius * radius * PI;
+    }
+
+    int ednakvi() {
+        if (perimetar() == plostina())
+            return 1;
+        else
+            return 0;
+    }
+};
+
+int main() {
+    float r;
+    cin >> r;
+    if (r >= 0) {
+        //instanciraj objekt od klasata Krug cij radius e vrednosta procitana od tastatura
+        Krug k(r);
+        cout << k.perimetar() << endl;
+        cout << k.plostina() << endl;
+        cout << k.ednakvi() << endl;
+    } else
+        cout << "Nevalidna vrednost za radius";
+    //instanciraj objekt od klasata Krug cij radius ne e definiran
+    Krug k1;
+    return 0;
+}
