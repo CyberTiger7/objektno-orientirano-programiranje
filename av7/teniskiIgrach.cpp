e5c0abf388553e803334732bcdf393cc9d3c65d9
Krstev <krstev.kostadin@gmail.com>
2017-04-04 19:37:10 +0200

    initial commit

---
+++ av7/teniskiIgrach.cpp
@@ -0,0 +1,85 @@
+//
+// Created by krstevkoki on 4/4/17.
+//
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+class TennisPlayer {
+private:
+    char name[100];
+    char surname[100];
+    bool playsInLeague;
+public:
+    TennisPlayer(const char *name = "", const char *surname = "", bool playsInLeague = false) {
+        strcpy(this->name, name);
+        strcpy(this->surname, surname);
+        this->playsInLeague = playsInLeague;
+    }
+
+    TennisPlayer(const TennisPlayer &tp) {
+        strcpy(this->name, tp.name);
+        strcpy(this->surname, tp.surname);
+        this->playsInLeague = tp.playsInLeague;
+    }
+
+    TennisPlayer &operator=(const TennisPlayer &tp) {
+        if (this != &tp) {
+            strcpy(this->name, tp.name);
+            strcpy(this->surname, tp.surname);
+            this->playsInLeague = tp.playsInLeague;
+        }
+        return *this;
+    }
+
+    friend ostream &operator<<(ostream &out, const TennisPlayer &tp) {
+        if (tp.playsInLeague == true)
+            return out << tp.name << " " << tp.surname << ", igra vo liga";
+        else
+            return out << tp.name << " " << tp.surname << ", ne igra vo liga";
+    }
+
+    bool PlaysInLeague() {
+        return playsInLeague;
+    }
+
+    void setPlaysInLeague(bool pl) {
+        this->playsInLeague = pl;
+    }
+};
+
+class RankedTennisPlayer : public TennisPlayer {
+private:
+    int rank;
+public:
+    RankedTennisPlayer(const char *name, const char *surname, bool playsInLeague = false, int rank = 0)
+            : TennisPlayer(name, surname, playsInLeague) {
+        this->rank = rank;
+    }
+
+    RankedTennisPlayer(const TennisPlayer &rtp, int rank) : TennisPlayer(rtp) {
+        this->rank = rank;
+    }
+
+    friend ostream &operator<<(ostream &out, const RankedTennisPlayer &rtp) {
+        out << TennisPlayer(rtp) << " " << rtp.rank;
+        return out;
+    }
+};
+
+int main() {
+    TennisPlayer rf("Roger", "Federer");
+    TennisPlayer ng("Novak", "Djokovikj");
+    cout << rf << endl;
+    cout << ng << endl;
+    //TennisPlayer t;
+    RankedTennisPlayer rn("Rafael", "Nadal", true, 2750);
+    cout << rn << endl;
+    TennisPlayer tp = rn;
+    cout << tp << endl;
+    //RankedTennisPlayer copy(tp);
+    RankedTennisPlayer copy(ng, 3320);
+    cout << copy << endl;
+    return 0;
+}
